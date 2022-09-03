123590922f3a2383f12e81dbc2b5dcf57dfeddf4
iejMac <kilianmaciej6@gmail.com>
2022-09-03 03:28:46 +0000

    run.sh: update to multi-node

--- encoder_distill/run.sh
+++ encoder_distill/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-torchrun --nproc_per_node 8 train.py \
+python3.8 train.py \
         --train-data "pipe:aws s3 cp s3://s-datasets/laion5b/laion2B-data/{000000..231348}.tar -" \
         --train-num-samples 2000000000 \
         --val-data "pipe:aws s3 cp s3://s-datasets/laion5b/laion2B-data/{231349..231349}.tar -" \
         --val-num-samples 10000 \
         --imagenet-val "/fsx/rom1504/imagenetval/imagenetvalv1" \
         --dataset-type "webdataset" \
-        --batch-size 256 \
-        --lr 1e-4 \
+        --batch-size 128 \
+        --lr 2e-4 \
         --beta1 0.9 \
         --beta2 0.98 \
         --eps 1e-6 \
@@ -20,4 +20,4 @@ torchrun --nproc_per_node 8 train.py \
         --save-frequency 1000 \
         --val-frequency 100 \
         --report-to "wandb" \
-        --name "H 256 bs 1e-4 lr long run" \
+        --name "H 2048 bs 2e-4 lr" \
