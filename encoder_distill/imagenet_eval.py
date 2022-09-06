69cbf3dc38362c5697ac0554dd37da765afdcd9f
iejMac <kilianmaciej6@gmail.com>
2022-09-06 04:19:41 +0000

    udpate

--- encoder_distill/imagenet_eval.py
+++ encoder_distill/imagenet_eval.py
@@ -14,7 +14,8 @@ model_kwargs = {"model_name": "ViT-H-14"}
 mlp_dims = (1024, 768)
 
 image_cpt = "checkpoints/H_65536_bs_1e-3_lr_10k_GPUh/step_10000.pt"
-text_cpt = "checkpoints/54k_text/step_54000.pt"
+text_cpt = "checkpoints/H_16384_bs_5e-4_lr/step_5000.pt"
+# text_cpt = "checkpoints/54k_text/step_54000.pt"
 
 args = parse_args()
 # discover initial world args early so we can log properly
@@ -22,7 +23,7 @@ args.distributed = False
 args.local_rank, args.rank, args.world_size = world_info_from_env()
 args.device = "cuda"
 
-model, preproc = put_together(model_type, model_kwargs, mlp_dims, image_cpt, text_cpt, args.device)
+model, preproc = put_together(model_type, model_kwargs, mlp_dims, image_cpt, text_cpt, False, args.device)
 model.to(args.device)
 data = get_data(args, (preproc, preproc))
 
--- encoder_distill/run.sh
+++ encoder_distill/run.sh
@@ -5,20 +5,19 @@ python3.8 train.py \
         --train-num-samples 2000000000 \
         --val-data "pipe:aws s3 cp s3://s-datasets/laion5b/laion2B-data/{231349..231349}.tar -" \
         --val-num-samples 10000 \
-        --imagenet-val "/fsx/rom1504/imagenetval/imagenetvalv1" \
         --dataset-type "webdataset" \
         --batch-size 256 \
-        --lr 7e-4 \
+        --lr 1e-3 \
         --beta1 0.9 \
         --beta2 0.98 \
         --eps 1e-6 \
         --wd 0.0 \
         --workers 6 \
         --epochs 10 \
-        --steps 100000 \
+        --steps 20000 \
         --warmup 1000 \
         --modality "image" \
         --save-frequency 1000 \
         --val-frequency 100 \
         --report-to "wandb" \
-        --name "H_16384_bs_7e-4_lr_10k_GPUh" \
+        --name "H_16384_bs_1e-3_lr" \
